02558615d425fda46e5654efcfe2cfadc291dc56
Luiz Tanure <letanure@gmail.com>
2022-07-05 22:55:24 +0200

    feat: add header

---
+++ src/ui/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Header = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="OLX3" width={71} height="41" />
+  </S.Wrapper>
+)
+
+export default Header
---
+++ src/ui/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  background-color: #002f34;
+  height: 88px;
+  padding: 16px 48px;
+`
+export const Logo = styled.img`
+  width: 72px;
+  height: 58px;
+`
