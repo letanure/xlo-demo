db5e586e3fcfb649a3eae61d30cc491659489dfc
Luiz Tanure <letanure@gmail.com>
2022-07-06 20:50:01 +0200

    chore: move locales to file

---
+++ src/components/Checkout/locales.ts
@@ -0,0 +1,92 @@
+export const languages: Languages = {
+  en: {
+    backText: 'Buy with delivery',
+    deliveryMethodsTitle: 'Delivery method',
+    deliveryMethodsSubTitle: 'Pickup at the selected point.',
+    termsAndConditionsText: '* Check the',
+    termsAndConditionsLink: 'terms and conditions',
+    yourDataTitle: 'Your data',
+    paymentMethodTitle: 'Payment method',
+    paymentMethodOption1:
+      'The item can be purchased using this payment method:',
+    paymentMethodBlik: 'Online payments - BLIK, Bank transfer',
+    supportUkraineTitle: '🇺🇦 Support Ukraine',
+    shoppingSecurity: 'Shopping security',
+    shoppingSecurityLabel: 'FREE',
+    shoppingSecuritySubTitle:
+      'Secure your purchases with OLX Shipment thanks to the Protection Package',
+    shoppingSecurityText1:
+      'To give you extra peace of mind, all purchases using OLX Shipments and OLX Payments are covered by the Protection Package. Thanks to this',
+    shoppingSecurityText2:
+      ', within 24 hours of receiving the package, you can:',
+    shoppingSecurityText3:
+      'Confirm the item received is the one you ordered before payment is sent to the seller',
+    shoppingSecurityText4:
+      'Report that you have received an empty package or that you have received an item other than what you ordered *. In this case, we suspend the payment and verify the application within 14 days. During this time, we may ask you for additional information.',
+    shoppingSecurityText5:
+      'If your application is successful, you will receive your money back',
+    shoppingSecurityText6:
+      'If your application is rejected, the payment will go to the seller',
+    shoppingSecurityText7:
+      '* The Protection Package does not include damaged, incomplete items, items showing signs of use, or items of a different color or size. In such cases, please contact the seller directly.',
+    shoppingSecurityText8:
+      'See the terms and conditions and the full list of exclusions',
+    paymentDetails: 'Payment details',
+    subject: 'Subject:',
+    shippingCost: 'Shipping cost:',
+    service: 'Service:',
+    totalCost: 'Total cost:',
+    pickupOn: 'Pick up at the Post Office, Żabka store or at the Orlen station',
+    pickApoint: 'Pick a poin',
+    actionCancel: 'Cancel',
+    actionNext: 'Next',
+    invoiceTitle: 'DO YOU WANT TO RECEIVE AN INVOICE FOR THE SERVICE?',
+    invoiceOption: 'Yes, I would like an invoice',
+    invoiceInfo:
+      '* If you want an invoice for a company, you must ask for it before purchasing'
+  },
+  pl: {
+    backText: 'Kup z dostawą',
+    deliveryMethodsTitle: 'Sposób dostawy',
+    deliveryMethodsSubTitle: 'Odbiór w wybranym punkcie.',
+    termsAndConditionsText: '* Cena promocyjna',
+    termsAndConditionsLink: 'Sprawdź warunki',
+    yourDataTitle: 'Twoje dane',
+    paymentMethodTitle: 'Metoda płatności',
+    paymentMethodOption1:
+      'Przedmiot można kupić przy użyciu tej metody płatności:',
+    paymentMethodBlik: 'Płatności online - BLIK, Przelew bankowy',
+    supportUkraineTitle: '🇺🇦 Wesprzyj Ukrainę',
+    shoppingSecurity: 'Bezpieczeństwo zakupów',
+    shoppingSecurityLabel: 'Za darmo',
+    shoppingSecuritySubTitle:
+      'Zabezpiecz swoje zakupy z Przesyłką OLX dzięki Pakietowi Ochronnemu',
+    shoppingSecurityText1:
+      'Aby zapewnić Ci dodatkowy spokój ducha, wszystkie zakupy z wykorzystaniem Przesyłki OLX i Płatności OLX objęte są Pakietem Ochronnym. Dzięki temu',
+    shoppingSecurityText2: 'w ciągu 24 godzin od odbioru paczki możesz:',
+    shoppingSecurityText3:
+      'Potwierdzić, że odebrany przedmiot jest tym, który zamówiłeś, zanim płatność zostanie wysłana do sprzedającego.',
+    shoppingSecurityText4:
+      'Zgłosić, że odebrałeś pustą paczkę lub przedmiot inny niż zamawiany*. W takim przypadku wstrzymujemy płatność i weryfikujemy zgłoszenie w ciągu 14 dni. W tym czasie możemy poprosić Cię o dodatkowe informacje.',
+    shoppingSecurityText5:
+      'Jeśli Twoje zgłoszenie zostanie uznane, otrzymasz zwrot pieniędzy',
+    shoppingSecurityText6:
+      'Jeśli Twoje zgłoszenie zostanie odrzucone, płatność trafi do sprzedającego',
+    shoppingSecurityText7:
+      '*Pakiet Ochronny nie obejmuje przedmiotów uszkodzonych, niekompletnych, noszących ślady używania, a także przedmiotów o innym kolorze lub rozmiarze. W takich przypadkach prosimy o kontakt bezpośrednio ze sprzedającym.',
+    shoppingSecurityText8: 'Zapoznaj się z warunkami i pełną listą wyłączeń',
+    paymentDetails: 'Szczegóły płatności',
+    subject: 'Przedmiot:',
+    shippingCost: 'Koszt przesyłki:',
+    service: 'Usługa serwisowa:',
+    totalCost: 'Całkowity koszt:',
+    pickupOn: 'Odbierz w Urzędzie Pocztowym, sklepie Żabka lub na stacji Orlen',
+    pickApoint: 'Wybierz punk',
+    actionCancel: 'Anuluj',
+    actionNext: 'Dalej',
+    invoiceTitle: 'CZY CHCESZ OTRZYMAĆ FAKTURĘ ZA USŁUGĘ SERWISOWĄ?',
+    invoiceOption: 'Tak, poproszę fakturę',
+    invoiceInfo:
+      '* Jeśli chcesz otrzymać fakturę na firmę, musisz poprosić o nią przed zakupem'
+  }
+}
