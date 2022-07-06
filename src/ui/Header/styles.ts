282b83466671881c65e753e8731f25ed779e33ec
Luiz Tanure <letanure@gmail.com>
2022-07-06 16:58:41 +0200

    feat: hide header on mobile

--- src/ui/Header/styles.ts
+++ src/ui/Header/styles.ts
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.header`
-  background-color: #002f34;
-  height: 88px;
-  padding: 16px 48px;
+export const Wrapper = styled.div`
+  ${() => css`
+    background-color: #002f34;
+    height: 88px;
+    padding: 16px 48px;
+    display: none;
+    ${media.greaterThan('medium')`
+      display: block;
+    `}
+  `}
 `
+
 export const Logo = styled.img`
   width: 72px;
   height: 58px;
