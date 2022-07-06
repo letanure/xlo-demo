82819c399261213e5bc96ec7b6c625365442ab26
Luiz Tanure <letanure@gmail.com>
2022-07-06 20:47:44 +0200

    chore: create component Price

---
+++ src/components/Checkout/Price.tsx
@@ -0,0 +1,19 @@
+import * as S from './styles'
+
+interface Props {
+  value: string
+  lineThrough?: boolean
+  hasAsterisk?: boolean
+}
+
+const Price = ({ value, lineThrough, hasAsterisk }: Props) => {
+  return (
+    <S.Price lineThrough={lineThrough}>
+      <S.PriceValue>{value}</S.PriceValue>
+      <S.PriceCurrency>zł</S.PriceCurrency>
+      {hasAsterisk && '*'}
+    </S.Price>
+  )
+}
+
+export default Price
