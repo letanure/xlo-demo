3aae32965535477ab8bc247ea7f04c2397d98cec
Luiz Tanure <letanure@gmail.com>
2022-07-07 14:20:33 +0200

    feat: form component handle changes and error

--- src/ui/Form/index.tsx
+++ src/ui/Form/index.tsx
@@ -1,15 +1,47 @@
+import { useEffect, useState } from 'react'
 import * as S from './styles'
-import FormField, { Props as FieldConfig } from '../FormField'
+import FormField, { Props as FieldConfig, FieldData } from '../FormField'
 
-export interface Props {
-  config: FieldConfig[]
+export type FormData = {
+  valid: boolean
+  fieldData: FieldData[]
 }
 
-const Form = ({ config }: Props) => {
+export interface Props {
+  config: Omit<FieldConfig, 'onChange'>[]
+  onChange: (data: FormData) => void
+}
+
+const Form = ({ config, onChange }: Props) => {
+  const [formData, setFormData] = useState<FieldData[]>([])
+
+  useEffect(() => {
+    onChange({
+      valid: formData.every((field) => field.valid),
+      fieldData: formData
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formData])
+
+  const handleOnChange = (newData: FieldData) => {
+    const dataExists = formData.find((data) => data.name === newData.name)
+    if (dataExists) {
+      setFormData((prevFormData) => {
+        return prevFormData.map((prevData) => {
+          if (prevData.name === newData.name) {
+            return newData
+          }
+          return prevData
+        })
+      })
+    } else {
+      setFormData((prevFormData) => [...prevFormData, newData])
+    }
+  }
   return (
     <S.Form>
       {config.map((fieldConfig, index) => (
-        <FormField key={index} {...fieldConfig} />
+        <FormField key={index} {...fieldConfig} onChange={handleOnChange} />
       ))}
     </S.Form>
   )
