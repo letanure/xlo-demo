7dd82f540a212d9717aab3a0a8a300e5d01102f6
Luiz Tanure <letanure@gmail.com>
2022-07-08 21:24:27 +0200

    refactor: fix position message error on desktop

--- src/ui/Message/styles.ts
+++ src/ui/Message/styles.ts
@@ -1,22 +1,30 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
-  background-color: #fff6d9;
-  color: #002f34;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 18px;
-  padding: 16px;
-  display: flex;
-  flex-wrap: nowrap;
-  align-content: center;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 16px;
+  ${() => css`
+    background-color: #fff6d9;
+    color: #002f34;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    padding: 16px;
+    display: flex;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 16px;
 
-  position: sticky;
-  bottom: 80px;
+    position: sticky;
+    bottom: 80px;
+    ${media.greaterThan('medium')`
+      bottom: 16px;
+      margin: 0 0 8px 0;
+    `}
+  `}
 `
+
 export const Icon = styled.img`
   width: 36px;
   height: 36px;
