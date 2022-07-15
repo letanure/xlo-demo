b676f8f575f000caeeeff3dabaec9b59149a595e
Luiz Tanure <letanure@gmail.com>
2022-07-15 13:14:34 +0200

    chore: add global styles / theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,24 +1,36 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  /* noto-sansregular - latin */
+  @font-face {
+    font-family: 'Geomanist Book';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''), url('/fonts/geomanist-book-webfont.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Geomanist Regular';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local(''), url('/fonts/geomanist-regular-webfont.woff2') format('woff2');
+  }
   * {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: Geomanist,Arial,sans-serif;
   }
-  html {
-    font-size: 16px;
-  }
-  html, body, #__next {
-    height: 100%;
-  }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: #f2f4f5;
-    line-height: 1.15;
-    -webkit-text-size-adjust: 100%;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 16px;
+    }
+    body {
+      font-family: ${theme.font.family.body.name};
+    }
+  `}
 `
 
 export default GlobalStyles
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,11 +6,152 @@ enum sizeOptions {
   xlarge = 'xlarge',
   xxlarge = 'xxlarge'
 }
-
 export type SizeOptions = `${sizeOptions}`
 
 export type Sizes = {
   [key in SizeOptions]: string
+}
+
+enum colorTypes {
+  primary = 'primary',
+  secondary = 'secondary',
+  positive = 'positive',
+  warning = 'warning',
+  negative = 'negative',
+  info = 'info',
+  light = 'light',
+  dark = 'dark'
+}
+export type ColorTypes = `${colorTypes}`
+
+type ColorDefinition = {
+  c050: string
+  c100: string
+  c200: string
+  c300: string
+  c400: string
+  c500: string
+  c600: string
+  c700: string
+  c800: string
+  c900: string
+  lightFrom: number
+}
+
+type ColorGroup = {
+  [key: string]: ColorDefinition
+}
+
+const grid = {
+  container: '130rem',
+  gutter: '3.2rem'
+}
+
+const fontSizes: Sizes = {
+  xsmall: '1.2rem',
+  small: '1.4rem',
+  medium: '1.6rem',
+  large: '1.8rem',
+  xlarge: '2.0rem',
+  xxlarge: '2.8rem'
+}
+
+const font = {
+  family: {
+    heading: {
+      local: true,
+      name: 'Geomanist Book',
+      src: '/fonts/geomanist-book-webfont.woff2',
+      weight: 400
+    },
+    body: {
+      local: true,
+      name: 'Geomanist Regular',
+      src: '/fonts/geomanist-regular-webfont.woff2',
+      weight: 400
+    }
+  },
+  size: fontSizes,
+  weight: {
+    light: 300,
+    normal: 400,
+    bold: 600
+  }
+}
+
+const color_black: ColorDefinition = {
+  c050: '#383838',
+  c100: '#363636',
+  c200: '#333333',
+  c300: '#2e2e2e',
+  c400: '#2c2c2c',
+  c500: '#272727',
+  c600: '#252525',
+  c700: '#232323',
+  c800: '#1e1e1e',
+  c900: '#121212',
+  lightFrom: 0
+}
+
+const color: ColorGroup = {
+  black: color_black
+}
+
+// Hover states are  one shade lighter on dark background, and one shade darker on light background.
+const color_semantic = {
+  elevation: {
+    e00: { bg: color.black.c050 },
+    e01: { bg: color.black.c100 },
+    e02: { bg: color.black.c200 },
+    e03: { bg: color.black.c300 },
+    e04: { bg: color.black.c400 },
+    e05: { bg: color.black.c500 },
+    e06: { bg: color.black.c600 },
+    e07: { bg: color.black.c700 },
+    e08: { bg: color.black.c800 },
+    e09: { bg: color.black.c900 }
+  },
+  text: {
+    body: {
+      default: { bg: color.black.c900 },
+      subdued: { bg: color.black.c600 },
+      link: { bg: color.black.c500 }
+    }
+  },
+  primary: {
+    disabled: {
+      value: color.black.c100,
+      inverse: color.black.c900
+    },
+    disabledPressed: {
+      value: color.black.c200,
+      inverse: color.black.c900
+    },
+    hover: {
+      value: color.black.c300,
+      inverse: color.black.c900
+    },
+    normal: {
+      value: color.black.c400,
+      inverse: color.black.c100
+    },
+    active: {
+      value: color.black.c500,
+      inverse: color.black.c100
+    }
+  }
+}
+
+const border = {
+  width: {
+    thin: '1px',
+    thick: '2px'
+  },
+  radius: {
+    small: '0.2rem',
+    medium: '0.4rem',
+    large: '0.6rem'
+  }
 }
 
 const spacing = {
@@ -24,5 +165,9 @@ const spacing = {
 }
 
 export default {
+  border,
+  color: color_semantic,
+  font,
+  grid,
   spacing
 }
